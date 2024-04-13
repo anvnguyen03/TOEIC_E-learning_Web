b49678e559a10acf4a3d631d8ab78e95f304e780
An <ThinkPad@DESKTOP-65TEKG7>
2024-04-13 16:45:27 +0700

    first commit

---
+++ TOEIC-learning-server/src/main/java/toeic/com/ToeicLearningServerApplication.java
@@ -0,0 +1,13 @@
+package toeic.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicLearningServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeicLearningServerApplication.class, args);
+	}
+
+}
---
+++ TOEIC-learning-server/src/test/java/toeic/com/ToeicLearningServerApplicationTests.java
@@ -0,0 +1,13 @@
+package toeic.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToeicLearningServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
